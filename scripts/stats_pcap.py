c52d769eff2051e1d0e1a5502f1857032c00b777
Andrey Quintero González <ingandreyq@gmail.com>
2020-02-09 23:36:44 -0300

    Obtain statistics from pcap
    Obtain statistics from a pcap generated 
    Use tshark to obtain Interval - Frames - Bytes

---
+++ scripts/stats_pcap.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+
+def run_tshark(in_pcap, out_txt):
+    command = "tshark -q -z 'io,stat,1' -r " + in_pcap + " > " + out_txt
+    os.system(command)
+
+def main():
+
+
+    parser = argparse.ArgumentParser(description="Obtain statistics from a PCAP file generated and outputs a TXT file containing " + 
+                                                 "Interval - Frames - Bytes")
+    parser.add_argument("-i", "--in_pcap", help="PCAP file to process")
+    parser.add_argument("-o", "--out_txt", help="TXT file to save results to")
+    args = parser.parse_args()
+
+    assert os.path.isfile(args.in_pcap)
+    assert not os.path.isfile(args.out_csv_gz) 
+        
+    run_tshark(args.in_pcap, args.out_txt) 
+        
+if __name__ == '__main__':
+    main()
